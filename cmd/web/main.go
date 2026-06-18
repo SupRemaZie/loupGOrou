3de8721c4e6143cddb5af21a9c8bb68b67f4bda1
SupRemaZie <dporchet1110@gmail.com>
2026-06-18 11:38:33 +0200

    web

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"path/filepath"
-	"runtime"
 
 	"github.com/SupRemaZie/loupGOrou/internal/server"
 )
 
 func main() {
-	// Serve the built React app from web/dist (relative to this file's directory)
-	_, file, _, _ := runtime.Caller(0)
-	root := filepath.Join(filepath.Dir(file), "..", "..", "web", "dist")
+	addr := flag.String("addr", ":8080", "adresse d'écoute")
+	static := flag.String("static", "./web/dist", "dossier du frontend compilé")
+	flag.Parse()
 
-	addr := ":8080"
-	log.Printf("Serveur démarré sur http://localhost%s", addr)
-
-	srv := server.New(addr, root)
+	log.Printf("Serveur démarré sur http://localhost%s", *addr)
+	srv := server.New(*addr, *static)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
